ec66b7f9205402ae82d97f836af1cbc8e241a2dc
Dennis Castro Mota <97762982+dennis-castro@users.noreply.github.com>
2022-02-03 18:23:15 -0300

    att resposta

--- tuplasComTimes.py
+++ tuplasComTimes.py
@@ -1,4 +1,5 @@
 from operator import index
+from re import T
 from uteis.str import titulo
 
 
@@ -23,6 +24,16 @@ print('*Times na zona de rebaixamento:')
 for c in range(0,4):
         print(f'{c+17} - {lista[c+16]}')
 print()
-t = str(input('Escreva o nome time, para ver sua colocação: '))
-print(f'{t} esta na {lista.index(t)+1}* colocação.')
 
+while True:
+        try:
+                t = str(input('Escreva o nome time, para ver sua colocação: '))
+                if t == '':
+                        print('O usuario nao informou nenhum time...')
+                        break
+                print(f'{t} esta na {lista.index(t)+1}* colocação.')
+                break
+        except:
+                print('Time não encontrado. Tente novamente...')
+    
+      
