204767c3bb0f9b93d4ee959bd658708c263d1da7
Dennis Castro Mota <97762982+dennis-castro@users.noreply.github.com>
2022-02-03 14:18:19 -0300

    att

---
+++ tuplasComTimes.py
@@ -0,0 +1,11 @@
+from UTEIS.utilidades import linha
+
+
+lista = (
+        'corinthians', 'palmeiras', 'santos', 'gremio',
+        'cruzeiro', 'flamengo', 'vasco', 'chapecoense',
+        'atletico', 'botafogo', 'atletico-pr', 'bahia',
+        'sao paulo', 'fluminense', 'sport', 'vitoria',
+        'coritiba', 'avai', 'ponte preta', 'atletico-go'
+       )
+print(linha)
