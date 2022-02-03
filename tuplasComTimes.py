a6f5b53735f07a3766c4d8922acc8bc348de4e74
Dennis Castro Mota <97762982+dennis-castro@users.noreply.github.com>
2022-02-03 17:36:05 -0300

    tuplas em python

--- tuplasComTimes.py
+++ tuplasComTimes.py
@@ -1,11 +1,28 @@
-from UTEIS.utilidades import linha
+from operator import index
+from uteis.str import titulo
 
 
+titulo('Tabela do Brasileirão 2021')
 lista = (
         'corinthians', 'palmeiras', 'santos', 'gremio',
         'cruzeiro', 'flamengo', 'vasco', 'chapecoense',
         'atletico', 'botafogo', 'atletico-pr', 'bahia',
         'sao paulo', 'fluminense', 'sport', 'vitoria',
         'coritiba', 'avai', 'ponte preta', 'atletico-go'
-       )
-print(linha)
+        )
+listaOrdemAlpha = sorted(lista)
+for t in listaOrdemAlpha:
+        print(t)
+
+print()
+print('*Os 5 primeiros colocados do brasileirão:')
+for c in range (0,5):
+        print(f'{c+1} - {lista[c]}')
+print()
+print('*Times na zona de rebaixamento:')
+for c in range(0,4):
+        print(f'{c+17} - {lista[c+16]}')
+print()
+t = str(input('Escreva o nome time, para ver sua colocação: '))
+print(f'{t} esta na {lista.index(t)+1}* colocação.')
+
---
+++ uteis/str.py
@@ -0,0 +1,11 @@
+from re import X
+
+
+def linha():
+    return ('-'*40)
+
+def titulo(msg):
+    tam = len(msg)+4
+    print('~'*tam)
+    print(f'  {msg}')
+    print('~'*tam)
