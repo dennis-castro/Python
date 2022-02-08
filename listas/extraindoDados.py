4e1ffbf49232795952ffe60aeb2df158251e752f
Dennis Castro Mota <97762982+dennis-castro@users.noreply.github.com>
2022-02-08 20:46:52 -0300

    ex listas

--- listas/extraindoDados.py
+++ listas/extraindoDados.py
@@ -1,7 +1,24 @@
-from re import T
-
-
+lista=[]
+cont = 0
 while True:
-    res = input('fale algo: ').strip().lower()[0]
-    
-    print(res)
+    num = int(input('Digite um numero: '))
+    if num not in lista:
+        lista.append(num)
+        print('Adicionado com sucesso...')
+    else:
+        print('Numero duplicado...')
+    res = str(input('Deseja continuar [S/N]: ')).upper().strip()[0]
+    while res not in 'SN':
+        res = str(input('Resposta invalida tente novamente [S/N]: ')).upper().strip()[0]
+    print('-='*30)
+    if res in 'N':
+        break
+        print('Finalizando...')
+print()
+lista.sort(reverse=True)
+print(f'Numeros digitados {lista}')
+print(f'Foram digitados {len(lista)} numeros.')
+if 5 in lista:
+    print('O numero 5 consta na lista.')
+else:
+    print('O numero 5 nao consta na lista.')
